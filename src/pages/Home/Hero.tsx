af011deeae7c723ae13b1bc6371f2c3de1a12d67
fahad <fah8ad@outlook.com>
2024-03-20 01:21:41 +0300

    add build

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -31,7 +31,6 @@ function Hero() {
 
       <div className="mt-6 flex justify-center -space-x-4 lg:justify-start">
         <img className="h-12 w-12 rounded-full ring ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
-        <img className="h-12 w-12 rounded-full ring ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
         <img className="h-12 w-12 rounded-full ring ring-white" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" />
         <div className="">
           <span className="pl-8 font-semibold">Customer Reviews</span>
